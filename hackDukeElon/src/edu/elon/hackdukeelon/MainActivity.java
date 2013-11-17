855ad426643ff905d7ab73d9b71e93b1078e535e
t-robbins <trobbins2@elon.edu>
2013-11-17 00:53:50 -0500

    adding own audio

--- hackDukeElon/src/edu/elon/hackdukeelon/MainActivity.java
+++ hackDukeElon/src/edu/elon/hackdukeelon/MainActivity.java
@@ -14,8 +14,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends BaseWizard {
 
+
+
+public class MainActivity extends BaseWizard {
+	private final String AUDIO_REMOVED = "/sdcard/noAudio.mp4";
+	private final String SONG_ADDED = "/sdcard/withSong.mp4";
+	private final String MERGED_VIDEO = "/sdcard/merge.mp4";
+	private final String COMPILATION = "/sdcard/mylist.txt";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,39 +29,83 @@ public class MainActivity extends BaseWizard {
 
 		Button b = (Button) findViewById(R.id.button1);
 
-		//mergeTest();
-
 		b.setOnClickListener(new OnClickListener(){
-
 			@Override
 			public void onClick(View arg0) {
 				compileVideos();
 			}
-
 		});
+		
+		
+		Button remove = (Button) findViewById(R.id.button2);
+		remove.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View arg0) {
+				dropAudio();
+			}
+		});
+		
+		Button add = (Button) findViewById(R.id.button3);
+		add.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				addAudio();
+			}
+		});
+		
+	}
+	
+	private void addAudio(){
+		copyLicenseAndDemoFilesFromAssetsToSDIfNeeded();
+		//setOutputFilePath("/sdcard/withSong.mp4");
+		String commandStr = "ffmpeg -i " + AUDIO_REMOVED + " -i /sdcard/song.mp3 -map 0 -map 1 -codec copy -shortest " + SONG_ADDED;
+		
+		setCommand(commandStr);
+		
+		setProgressDialogTitle("Adding audio!");
+		setProgressDialogMessage("Depends on your video size, it can take a few minutes");
+		setNotificationTitle("HackDuke Application");
+		setNotificationfinishedMessageTitle("Audio added!");
+
+		runTranscoing();
+	}
+	
+	private void dropAudio(){
+		copyLicenseAndDemoFilesFromAssetsToSDIfNeeded();
+
+		String commandStr = "ffmpeg -i " + MERGED_VIDEO + "-vcodec copy -an " + AUDIO_REMOVED;
+	
+		setCommand(commandStr);
+		
+		setProgressDialogTitle("Removing audio!");
+		setProgressDialogMessage("Depends on your video size, it can take a few minutes");
+		setNotificationTitle("HackDuke Application");
+		setNotificationfinishedMessageTitle("Audio all removed");
+
+		runTranscoing();
+		
 	}
 
 	private void compileVideos(){
 
 		copyLicenseAndDemoFilesFromAssetsToSDIfNeeded();
 
-		String commandStr = "ffmpeg -f concat -i /sdcard/mylist.txt -c copy /sdcard/merge.mp4";
+		String commandStr = "ffmpeg -f concat -i " + COMPILATION + " -c copy " + MERGED_VIDEO;
 		
 
 		setCommand(commandStr);
 
-		///optional////
+	
 		setProgressDialogTitle("Compiling your music video!");
 		setProgressDialogMessage("Depends on your video size, it can take a few minutes");
-		//setNotificationIcon(R.drawable.icon2);
+
 		setNotificationTitle("HackDuke Application");
 		setNotificationfinishedMessageTitle("Compiling finished");
 		setNotificationfinishedMessageDesc("Click to play demo");
 		setNotificationStoppedMessage("Demo Transcoding stopped");
-		///////////////
 
 		runTranscoing();
-		///////////////////////////////////////////////////////////////////////////////
 	}
 
 	@Override
