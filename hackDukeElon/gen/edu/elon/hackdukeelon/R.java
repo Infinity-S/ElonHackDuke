ed8df78aa4afdeecc19c5f6d12c39d47b3d875b0
Schuyler Goodwin <schuylergoodwin@gmail.com>
2013-11-16 21:15:53 -0500

    First Commit for Schuyler

--- hackDukeElon/gen/edu/elon/hackdukeelon/R.java
+++ hackDukeElon/gen/edu/elon/hackdukeelon/R.java
@@ -17,25 +17,97 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int notification_icon=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int CommandText=0x7f080005;
+        public static final int CommandTextView=0x7f080004;
+        public static final int ScrollView01=0x7f080003;
+        public static final int aboutButton=0x7f08000e;
+        public static final int about_content=0x7f080000;
+        public static final int action_settings=0x7f080014;
+        public static final int contactButton=0x7f08000d;
+        public static final int contact_us_content=0x7f080002;
+        public static final int del1=0x7f080007;
+        public static final int invokeButton=0x7f080006;
+        public static final int license=0x7f080009;
+        public static final int list=0x7f080001;
+        public static final int purchase=0x7f08000c;
+        public static final int showFileDone=0x7f080011;
+        public static final int showFileSend=0x7f080012;
+        public static final int showFileTitle1=0x7f08000f;
+        public static final int showFileTitle2=0x7f080010;
+        public static final int showFiletextView1=0x7f080013;
+        public static final int showLastRunLogButton=0x7f080008;
+        public static final int space1=0x7f08000b;
+        public static final int startDemoAct=0x7f08000a;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int about_dialog=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int contact_us_dialog=0x7f030002;
+        public static final int ffmpeg_demo_client2=0x7f030003;
+        public static final int remoteserviceclient=0x7f030004;
+        public static final int show_file=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int FfmpegStatus=0x7f040009;
+        public static final int RemoteProcessStatus=0x7f040008;
+        public static final int aboutButton=0x7f040015;
+        public static final int about_email_button=0x7f040010;
+        public static final int about_email_content=0x7f040012;
+        public static final int about_email_subject=0x7f040011;
+        public static final int about_email_uri=0x7f040013;
+        public static final int action_settings=0x7f04002d;
+        public static final int app_name=0x7f040001;
+        public static final int bindButton=0x7f040004;
+        public static final int bitrate_invalid_message=0x7f04001c;
+        public static final int commandText=0x7f04000e;
+        public static final int commandTextView=0x7f04000d;
+        public static final int contactButton=0x7f040016;
+        public static final int dialog_error_title=0x7f040019;
+        public static final int getWorkerStatusButton=0x7f040007;
+        public static final int hello_world=0x7f04002e;
+        public static final int input_file_contains_spaces_message=0x7f04001a;
+        public static final int invokeButton=0x7f040006;
+        public static final int license=0x7f04000a;
+        public static final int notif_message_fail=0x7f04001e;
+        public static final int notif_message_ok=0x7f04001d;
+        public static final int notif_message_stopped=0x7f040020;
+        public static final int notif_message_stopping=0x7f04001f;
+        public static final int notif_progress_desc=0x7f040022;
+        public static final int notif_progress_title=0x7f040021;
+        public static final int notif_start_desc=0x7f040024;
+        public static final int notif_start_title=0x7f040023;
+        public static final int oem_license=0x7f040018;
+        public static final int output=0x7f04000f;
+        public static final int output_file_contains_spaces_message=0x7f04001b;
+        public static final int progress_dialog_message=0x7f04002c;
+        public static final int progress_dialog_title=0x7f04002b;
+        public static final int purchase=0x7f040017;
+        public static final int releaseButton=0x7f040005;
+        public static final int show_file_text=0x7f040027;
+        public static final int show_file_title1=0x7f040025;
+        public static final int show_file_title2=0x7f040026;
+        public static final int show_logs=0x7f040028;
+        public static final int startButton=0x7f040002;
+        public static final int startDemoAct=0x7f04000b;
+        public static final int stopButton=0x7f040003;
+        public static final int trascoding_running_background_message=0x7f04002a;
+        public static final int trascoding_running_message=0x7f040029;
+        public static final int ui_ok=0x7f040014;
+        public static final int welcome=0x7f040000;
+        public static final int welcome_demo=0x7f04000c;
     }
     public static final class style {
         /** 
@@ -59,10 +131,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050003;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050004;
+        public static final int h1=0x7f050001;
+        public static final int h2=0x7f050002;
+        public static final int t1=0x7f050000;
     }
 }
--- hackDukeElon/src/edu/elon/hackdukeelon/MainActivity.java
+++ hackDukeElon/src/edu/elon/hackdukeelon/MainActivity.java
@@ -1,17 +1,50 @@
 package edu.elon.hackdukeelon;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+
 import com.netcompss.ffmpeg4android_client.BaseWizard;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends BaseWizard {
+	private ListView songSegements; 
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		songSegements = (ListView) findViewById(R.id.list); 
+		songSegements.setOnItemClickListener(clickListener); 
+		
+		updateList();
+	}
+	
+	private OnItemClickListener clickListener = new OnItemClickListener() {
+		/*Clicking on list items */
+		@Override
+		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+				long arg3) {
+			
+			
+		}
+	};
+	
+	private void updateList() {
+		String[] secondsList = {"1 Second", "5 Second", "10 second"}; 
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, secondsList); 
 	}
 
 	@Override
